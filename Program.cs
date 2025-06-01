41740089ff0a42bc8e5bea84981fa992bc948d72
wmcsantos <wilsoncavacosantos@gmail.com>
2025-06-01 23:18:03 +0100

    Project Hackett London API Boilerplate

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorc"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/colors", async () =>
