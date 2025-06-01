bee50ee6b9bd8f143aea103c03b419ce30fdb330
wmcsantos <wilsoncavacosantos@gmail.com>
2025-06-01 23:03:26 +0100

    Project Hackett London API Boilerplate

---
+++ Color.cs
@@ -0,0 +1,7 @@
+public class Color
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public string ImageUrl { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+using MySqlConnector;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/colors", async () =>
+{
+    var colors = new List<Color>();
+
+    using var conn = new MySqlConnection(connectionString);
+    await conn.OpenAsync();
+
+    var cmd = new MySqlCommand("SELECT id, name, code, image_url FROM colors;", conn);
+    using var reader = await cmd.ExecuteReaderAsync();
+
+    while (await reader.ReadAsync())
+    {
+        colors.Add(new Color
+        {
+            Id = reader.GetInt32(0),
+            Name = reader.GetString(1),
+            Code = reader.GetString(2),
+            ImageUrl = reader.GetString(3)
+        });
+    }
+
+    return Results.Ok(colors);
+});
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
